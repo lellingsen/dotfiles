88d681a3667b88e2ef2af52ed6acc8ccd8aa8beb
Lars Ellingsen <lars.ellingsen@dashlane.com>
2022-09-13 10:31:40 -0700

    removing eslint from null-ls

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -4,12 +4,10 @@ if not null_ls_status_ok then
 end
 
 local formatting = null_ls.builtins.formatting
-local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
   debug = false,
   sources = {
     formatting.prettier,
-    diagnostics.eslint_d,
   },
 })
