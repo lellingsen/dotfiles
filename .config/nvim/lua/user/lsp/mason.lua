0b6d2c5d2c40160e24eb712b81da17de82e9b98a
Lars Ellingsen <lars.ellingsen@dashlane.com>
2023-03-10 09:45:54 -0800

    add rust to ensure installed list

--- .config/nvim/lua/user/lsp/mason.lua
+++ .config/nvim/lua/user/lsp/mason.lua
@@ -1,4 +1,4 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "sumneko_lua", "tsserver", "jsonls" }
+  ensure_installed = { "sumneko_lua", "tsserver", "jsonls", "rust_analyzer" }
 })
