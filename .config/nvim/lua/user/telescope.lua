cbdcb96a49e9a0d53fd7ad4d174083ba9fea00e9
Lars Ellingsen <lars.ellingsen@dashlane.com>
2023-04-13 14:00:31 -0700

    make telescope preview smaller

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -6,13 +6,18 @@ end
 telescope.setup {
   defaults = {
     prompt_prefix = "  ",
+    layout_config = {
+      horizontal = {
+        preview_width = 0.3
+      },
+    },
   },
   extensions = {
     fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
     }
   },
   pickers = {
