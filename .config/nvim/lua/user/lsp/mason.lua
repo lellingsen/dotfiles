cd0d51b3c268997de6a8324a61ba5339d56389e0
Lars Ellingsen <lars@Larss-MacBook-Pro.local>
2023-06-10 09:59:06 -0700

    removing sumneko lua

--- .config/nvim/lua/user/lsp/mason.lua
+++ .config/nvim/lua/user/lsp/mason.lua
@@ -1,4 +1,4 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "sumneko_lua", "tsserver", "jsonls", "rust_analyzer" }
+  ensure_installed = { "tsserver", "jsonls", "rust_analyzer" }
 })
