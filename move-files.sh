18928d416953391481fb7f3cf075f47b81368b6b
Lars Ellingsen <lars@Larss-MacBook-Pro.local>
2023-07-11 07:54:50 -0700

    add more files to main copy script

--- move-files.sh
+++ move-files.sh
@@ -2,3 +2,7 @@
 
 cp ./.vimrc ~/.vimrc
 cp -a ./.config/. ~/.config/
+cp ./.zshrc ~/.zshrc
+cp ./.tmux.conf ~/.tmux.conf
+cp ./headline.zsh-theme $ZSH_CUSTOM/themes/headline.zsh-theme
+cp ./.wezterm.lua ~/.wezterm.lua
