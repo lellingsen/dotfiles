097cfb7d4a696c7e2601de32795f4791bda284fe
Lars Ellingsen <lars@datavant.com>
2023-07-12 09:18:49 -0700

    change tab width to 4

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -1,8 +1,8 @@
 local opt = vim.opt
 
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
 opt.expandtab = true
 opt.smartindent = true
 opt.autoindent = true
