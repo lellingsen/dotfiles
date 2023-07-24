27922c1e285d8522227ce298a32dc2b7c72d39e1
Lars Ellingsen <lars@datavant.com>
2023-07-24 09:35:28 -0700

    add pyright to list of lsps

--- .config/nvim/lua/user/lsp/mason.lua
+++ .config/nvim/lua/user/lsp/mason.lua
@@ -1,4 +1,4 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "tsserver", "jsonls", "rust_analyzer" }
+  ensure_installed = { "tsserver", "jsonls", "rust_analyzer", "pyright" }
 })
