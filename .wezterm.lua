36258bd94c0daf8cba645124178f731e66b5b476
Lars Ellingsen <lars@datavant.com>
2023-12-06 14:43:38 -0800

    Switch to nordfox theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,7 +11,7 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-config.color_scheme = 'nord'
+config.color_scheme = 'nordfox' --'GruvboxDark', 'nord', 'Catppuccin Macchiato (Gogh)', 'nordic' (added custom)
 config.font_size = 15
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
@@ -20,7 +20,7 @@ config.window_padding = {
   top = 0,
   bottom = 0,
 }
-config.window_background_opacity = 0.85
+--config.window_background_opacity = 0.85
 config.adjust_window_size_when_changing_font_size = false
 config.keys = {
     -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
