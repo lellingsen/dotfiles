69ed8c434d7009921dd6c89cc9bd0b4069d765a8
Lars Ellingsen <lars@datavant.com>
2023-12-06 14:43:11 -0800

    Remove Kotlin server

--- .config/nvim/lua/user/lsp/mason.lua
+++ .config/nvim/lua/user/lsp/mason.lua
@@ -1,4 +1,4 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "tsserver", "jsonls", "rust_analyzer", "pyright", "kotlin-language-server" }
+  ensure_installed = { "tsserver", "jsonls", "rust_analyzer", "pyright" }
 })
