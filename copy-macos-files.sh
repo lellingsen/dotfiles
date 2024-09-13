d3885e7f9044c6fe753c79225f7ca953952e945b
Lars Ellingsen <lars@datavant.com>
2024-09-13 09:36:46 -0700

    Move theme to new file name to avoid conflict; make it two lines

--- copy-macos-files.sh
+++ copy-macos-files.sh
@@ -5,5 +5,5 @@ cp -a ./.config/. ~/.config/
 cp ./.zshrc ~/.zshrc
 cp ./.tmux.conf ~/.tmux.conf
 cp ./headline.zsh-theme ~/.oh-my-zsh/custom/themes/headline.zsh-theme
-cp ./robbyrussell.zsh-theme ~/.oh-my-zsh/custom/themes/robbyrussell.zsh-theme
+cp ./robbyrussell-modified.zsh-theme ~/.oh-my-zsh/custom/themes/robbyrussell-modified.zsh-theme
 cp ./.wezterm.lua ~/.wezterm.lua
--- robbyrussell-modified.zsh-theme
+++ robbyrussell-modified.zsh-theme
@@ -1,7 +1,8 @@
 PROMPT='$(virtualenv_prompt_info)'
-PROMPT+="%(?:%{$fg_bold[green]%}%1{➜%} :%{$fg_bold[red]%}%1{➜%} )"
 PROMPT+="%{$fg[cyan]%}%c%{$reset_color%}"
 PROMPT+=' $(git_prompt_info)'
+PROMPT+="
+%(?:%{$fg_bold[green]%}%1{➜%} :%{$fg_bold[red]%}%1{➜%} )"
 
 ZSH_THEME_VIRTUALENV_PREFIX="%{$fg_bold[yellow]%}("
 ZSH_THEME_VIRTUALENV_SUFFIX=")%{$reset_color%} "
