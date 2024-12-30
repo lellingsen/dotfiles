2daeefc60bb924f5db3bca413d6ba0eb1b132809
Lars Ellingsen <lars@datavant.com>
2024-12-30 09:03:53 -0800

    Add Ghostty config

--- copy-macos-files.sh
+++ copy-macos-files.sh
@@ -7,3 +7,4 @@ cp ./.tmux.conf ~/.tmux.conf
 cp ./headline.zsh-theme ~/.oh-my-zsh/custom/themes/headline.zsh-theme
 cp ./robbyrussell-modified.zsh-theme ~/.oh-my-zsh/custom/themes/robbyrussell-modified.zsh-theme
 cp ./.wezterm.lua ~/.wezterm.lua
+cp ./ghostty-config ~/Library/Application\ Support/com.mitchellh.ghostty/config
