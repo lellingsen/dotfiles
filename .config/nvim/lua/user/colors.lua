a92b0fdf666846ed3aa735df9078e326fb41865b
Lars Ellingsen <lars@datavant.com>
2025-05-13 16:54:16 -0700

    refactor(nvim): Move lualine config into colors file

--- .config/nvim/lua/user/colors.lua
+++ .config/nvim/lua/user/colors.lua
@@ -41,3 +41,35 @@ if not status_ok then
     vim.notify("colorscheme " .. colorscheme .. " not found!")
     return
 end
+
+local lualine_status_ok, lualine = pcall(require, "lualine")
+if not lualine_status_ok then
+  return
+end
+
+lualine.setup({
+  options = {
+    theme = colorscheme,
+  },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+      }
+    },
+    lualine_x = {
+    },
+    lualine_y = {
+      'filetype'
+    },
+  },
+  inactive_sections = {
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+      }
+    },
+  },
+})
