87edd4892bcc051e3254b2b9349854c4d91c3f19
Lars Ellingsen <lars@datavant.com>
2026-04-02 15:30:54 -0700

    disable ts_ls from formatting

--- .config/nvim/lua/user/lsp/mason.lua
+++ .config/nvim/lua/user/lsp/mason.lua
@@ -1,4 +1,17 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "ts_ls", "jsonls", "rust_analyzer", "ruff" }
+  ensure_installed = { "ts_ls", "jsonls", "rust_analyzer", "ruff" },
+  handlers = {
+    function(server_name)
+      require("lspconfig")[server_name].setup({})
+    end,
+    ["ts_ls"] = function()
+      require("lspconfig").ts_ls.setup({
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+        end,
+      })
+    end,
+  },
 })
